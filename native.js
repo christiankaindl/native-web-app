c85dde5b640212eeb341570e5637f9cb39a34e7a
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-08-23 16:20:07 +0200

    Create export function
    and add dblclick preventer

--- native.js
+++ native.js
@@ -1,3 +1,13 @@
-document.addEventListener('gesturestart', function (event) {
-    event.preventDefault();
-}, false);
+function init () {
+  // Prevent zooming in Safari
+  document.addEventListener('gesturestart', (event) => {
+    event.preventDefault()
+  }, false)
+
+  // Disable double-tap to zoom
+  document.addEventListener('dblclick', (event) => {
+    event.preventDefault()
+  })
+}
+
+export default init
